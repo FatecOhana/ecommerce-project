f6e41c85f410731e56540beeb9ee5492a273306c
Guilherme Palma <guippalma@gmail.com>
2023-11-09 21:44:30 -0300

    feat: add product class

---
+++ src/main/java/com/ecommerce/database/models/entities/Product.java
@@ -0,0 +1,40 @@
+package com.ecommerce.database.models.entities;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    private Integer categoryId;
+
+    private Integer weight;
+    private Integer height;
+    private Integer width;
+    private Integer price;
+    private Integer stock;
+
+    private String desc;
+    private String unit;
+    private String brand;
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+
+}
